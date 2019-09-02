6b68d3a564498587f8c9db5397c6d5447c1efa42
Rick Gigger <rickg@gmail.com>
2019-09-02 10:40:18 -0600

    Apparently using a - in a package name basically breaks everything

--- Tests/database-kitTests/database_kitTests.swift
+++ Tests/database-kitTests/database_kitTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import database_kit
+@testable import DatabaseKit
 
 final class database_kitTests: XCTestCase {
     
--- Tests/database-kitTests/marksTests.swift
+++ Tests/database-kitTests/marksTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import database_kit
+@testable import DatabaseKit
 
 final class marksTests: XCTestCase {
     
