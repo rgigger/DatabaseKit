d353fe2ea8f8f75e4e8b0cb8b512ba90e038ad18
Rick Gigger <rickg@gmail.com>
2019-09-02 10:43:54 -0600

    still more openness required

--- Sources/DatabaseKit/base.swift
+++ Sources/DatabaseKit/base.swift
@@ -17,15 +17,15 @@ open class BaseIndex {
         self.store.createCollection(name)
         self.collection = self.store.getCollection(name)!
     }
-    func get(_ key: String) -> String? {
+    public func get(_ key: String) -> String? {
         guard let loaded = self.collection.get(key: key) else { return nil }
         return String(data: loaded, encoding: .utf8)
     }
-    func set(key: String, value: String) {
+    public func set(key: String, value: String) {
         // it is supposedly safe to do a force unwrap here as long as the encoding is .utf8
         self.collection.set(key: key, data: value.data(using: .utf8)!)
     }
-    func delete(key: String) {
+    public func delete(key: String) {
         self.collection.delete(key: key)
     }
 }
