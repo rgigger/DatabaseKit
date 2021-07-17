7a0da5144317b064eae261345a7e1cc9e0ece271
Rick Gigger <rick@gigger.net>
2021-07-17 00:46:34 -0600

    add DictCollection

--- Sources/DatabaseKit/DictCollection.swift
+++ Sources/DatabaseKit/DictCollection.swift
@@ -9,24 +9,27 @@ import Foundation
 import SwiftLMDB
 
 class DictCollection: SimpleCollection {
-    let name: String
-    var docs: [String:Data] = [:]
-    init(_ name: String) {
-        self.name = name
-    }
-    func set(key: String, data: Data) {
-        self.docs[key] = data
-    }
-    func get(key: String) -> Data? {
-        return self.docs[key]
-    }
-    func delete(key: String) {
-        self.docs.removeValue(forKey: key)
-    }
-    func each(_ cb: (String, Data) -> Bool) {
-        for (key, data) in self.docs {
-            let keepGoing = cb(key, data)
-            if(!keepGoing) { break }
-        }
-    }
+   let name: String
+   var docs: [String:Data] = [:]
+   init(_ name: String) {
+       self.name = name
+   }
+   func set(key: String, data: Data) {
+       self.docs[key] = data
+   }
+   func get(key: String) -> Data? {
+       return self.docs[key]
+   }
+   func delete(key: String) {
+       self.docs.removeValue(forKey: key)
+   }
+   func each(_ cb: (String, Data) -> Bool) {
+       for (key, data) in self.docs {
+           let keepGoing = cb(key, data)
+           if(!keepGoing) { break }
+       }
+   }
+   func empty() throws {
+       docs = [:]
+   }
 }
