84d8f34968d27fef7a065990203d62650cf68275
Rick Gigger <rick@gigger.net>
2022-03-30 01:47:51 -0600

    Update to rgigger/SwiftLMDB 2.20

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "DatabaseKit",
     platforms: [
         .macOS(.v11),
-        .iOS(.v14),
+        .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -16,8 +16,8 @@ let package = Package(
             targets: ["DatabaseKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/rgigger/SwiftLMDB", .branch("master")),
+        .package(url: "https://github.com/rgigger/SwiftLMDB", revision: "2.2.0")
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/DatabaseKit/LmdbCollection.swift
+++ Sources/DatabaseKit/LmdbCollection.swift
@@ -19,31 +19,41 @@ public class LmdbCollection: SimpleCollection {
         self.database = try environment.openDatabase(named: name, flags: [.create])
     }
     public func set(key: String, data: Data, withTransaction optionalLmdbTransaction: Transaction?) throws {
-        try database.put(value: data, forKey: key, withTransaction: optionalLmdbTransaction?.transaction)
+        if let transaction = optionalLmdbTransaction?.transaction {
+            try database.put(value: data, forKey: key, withTransaction: transaction)
+        } else {
+            try database.put(value: data, forKey: key)
+        }
     }
     public func get(key: String, withTransaction optionalLmdbTransaction: Transaction?) throws -> Data? {
-        return try database.get(type: Data.self, forKey: key, withTransaction: optionalLmdbTransaction?.transaction)
+        if let transaction = optionalLmdbTransaction?.transaction {
+            return try database.get(type: Data.self, forKey: key, withTransaction: transaction)
+        } else {
+            return try database.get(type: Data.self, forKey: key)
+        }
     }
     public func delete(key: String, withTransaction optionalLmdbTransaction: Transaction?) throws {
-        try database.deleteValue(forKey: key, withTransaction: optionalLmdbTransaction?.transaction)
+        if let transaction = optionalLmdbTransaction?.transaction {
+            try database.deleteValue(forKey: key, withTransaction: transaction)
+        } else {
+            try database.deleteValue(forKey: key)
+        }
+
     }
     
     public func each(withTransaction optionalLmdbTransaction: Transaction?, _ cb: (String, Data) -> Bool) throws {
         
-        func readEach(_ swiftLMDBTransaction: SwiftLMDB.Transaction, _ callback: (String, Data) -> Bool) throws {
-            let cursor = try database.createCursor(transaction: swiftLMDBTransaction)
-            var (keyData, valData) = try cursor.first()
-            while let kd = keyData, let vd = valData {
-                let keyString = String(data: kd)!
-                _ = callback(keyString, vd)
-                (keyData, valData) = try cursor.next()
+        func readEach(_ swiftLMDBTransaction: SwiftLMDBTransaction, _ callback: (String, Data) -> Bool) throws {
+            for (keyData, valData) in try database.cursor(withTransaction: swiftLMDBTransaction) {
+                let keyString = String(data: keyData)!
+                _ = callback(keyString, valData)
             }
         }
         
         if let lmdbTransaction = optionalLmdbTransaction {
             try readEach(lmdbTransaction.transaction, cb)
         } else {
-            try environment.read { (swiftLMDBTransaction) -> SwiftLMDB.Transaction.Action in
+            try environment.read { (swiftLMDBTransaction) -> SwiftLMDBTransaction.Action in
                 try readEach(swiftLMDBTransaction, cb)
                 return .commit
             }
